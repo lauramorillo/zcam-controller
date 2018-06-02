959239e7e3e6e223da4138b73ecb8ffd8e903f35
Laura Morillo-Velarde <laura.morillovelarde@gmail.com>
2018-06-03 01:42:19 +0200

    Reloading config every minute

--- run_job.js
+++ run_job.js
@@ -104,7 +104,7 @@ async function loadConfig() {
   }
 }
 
-configJob = schedule.scheduleJob('*/5 * * * *', () => {
+configJob = schedule.scheduleJob('*/1 * * * *', () => {
   loadConfig().catch(err => trace.error(err))
 });
 loadConfig().catch(err => trace.error(err))
