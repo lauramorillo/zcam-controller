68599e01c899f692a91459e441b42dcf3e63267b
Laura Morillo-Velarde <laura.morillovelarde@gmail.com>
2018-09-22 14:14:41 +0200

    Fixing wrong param.

--- controllers/Cam.js
+++ controllers/Cam.js
@@ -7,7 +7,7 @@ const camConnector = new CamConnector(tracer)
 class CamController {
   static executeCommand(req, res, next) {
     const path = req.path.substring(4, req.path.length)
-    camConnector.executeCommant(path, query).then(result => {
+    camConnector.executeCommant(path, req.query).then(result => {
       res.json(result)
     }).catch(err => {
       res.json(error)
